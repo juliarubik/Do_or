78689d3460b7012f286cf372690ee1e440bbae19
juliarubik <43538276+juliarubik@users.noreply.github.com>
2019-02-13 13:49:39 +0300

    Add files via upload

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,86 +2,66 @@
 
 public class Door
 {
-    private GameObject DoorController;
+    private GameObject doorController;
 
-    private GameObject DoorBody;
+    private GameObject doorBody;
 
     private float rotation;
 
     private float speed;
 
-    private bool left;
-
     private bool isOpen;
 
-    public Door(bool left, bool isOpen, float rotation = 120, float speed = 5, string door_controller = "DoorController", 
-    string door_body = "DoorBody")
+    private bool isLeft;
+
+    public Door
+        (
+        bool isLeft, 
+        bool isOpen, 
+        float rotation = 120, 
+        float speed = 5, 
+        string door_controller = "DoorController", 
+        string door_body = "DoorBody"
+        )
     {
         this.rotation = rotation;
         this.speed = speed;
-        this.left = left;
         this.isOpen = isOpen;
-        DoorController = Object.Instantiate(Resources.Load(door_controller, typeof(GameObject))) as GameObject;
-        DoorBody = Object.Instantiate(Resources.Load(door_body, typeof(GameObject))) as GameObject;
-        Movement();
-        SetParent();
-    }
-
-    // Сдвиг оси вращения.
-    public void Movement()
-    {
-        if (left)
-        {
-            LeftShift();
-        }
-        else
-        {
-            RightShift();
-        }
+        doorController = Object.Instantiate(Resources.Load(door_controller, typeof(GameObject))) as GameObject;
+        doorBody = Object.Instantiate(Resources.Load(door_body, typeof(GameObject))) as GameObject;
+        Shift(isLeft);
+        doorBody.transform.parent = doorController.transform;
     }
 
     public void OpenClose()
     {
-        if (isOpen)
-        {
-            Open();
-        }
-        else
-        {
-            Close();
-        }
+        rotation = isOpen ? rotation : 0;
+
+        doorController.transform.rotation = Quaternion.Slerp
+        (
+            doorController.transform.rotation,
+            Quaternion.Euler
+            (
+                doorController.transform.rotation.x,
+                rotation,
+                doorController.transform.rotation.z
+            ),
+            speed * Time.deltaTime
+        );
     }
 
-    // Сдвиг пустышки влево.
-    private void LeftShift()
+    // Сдвиг оси вращения.
+    private void Shift(bool isLeft)
     {
-        float width = DoorBody.transform.localScale.z / 2;
-        DoorController.transform.position = new Vector3(DoorController.transform.position.x, DoorController.transform.position.y, 
-        DoorController.transform.position.z - width);
-    }
+        float width = doorBody.transform.localScale.z / 2;
 
-    // Сдвиг пустышки вправо.
-    private void RightShift()
-    {
-        float width = DoorBody.transform.localScale.z / 2;
-        DoorController.transform.position = new Vector3(DoorController.transform.position.x, DoorController.transform.position.y, 
-        DoorController.transform.position.z + width);
-    }
+        width = isLeft ? ((-1) * width) : width;
 
-    private void Open()
-    {
-        DoorController.transform.rotation = Quaternion.Slerp(DoorController.transform.rotation, Quaternion.Euler(DoorController.transform.rotation.x, 
-        rotation, DoorController.transform.rotation.z), speed * Time.deltaTime);
-    }
-
-    private void Close()
-    {
-        DoorController.transform.rotation = Quaternion.Slerp(DoorController.transform.rotation, Quaternion.Euler(DoorController.transform.rotation.x,
-        0, DoorController.transform.rotation.z), speed * Time.deltaTime);
-    }
-
-    public void SetParent()
-    {
-        DoorBody.transform.parent = DoorController.transform;
+        doorController.transform.position = new Vector3
+        (
+            doorController.transform.position.x, 
+            doorController.transform.position.y, 
+            doorController.transform.position.z + width
+        );
     }
 }
